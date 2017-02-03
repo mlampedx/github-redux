cb60a251379e7766d46d1a53b72ec70ef9ce525e
mlampedx <mlampesd@gmail.com>
2017-02-02 21:11:36 -0800

    Changed React mount point from App to Root

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import App from './containers/App';
+import Root from './containers/Root';
 import configureStore from './redux/configureStore';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <App store={store} history={history} />,
+  <Root store={store} history={history} />,
   document.querySelector('#root')
 );
