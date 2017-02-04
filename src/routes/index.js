0f5e45062809d9f3ad73b37db42fca484de8d23a
mlampedx <mlampesd@gmail.com>
2017-02-03 23:19:33 -0800

    Altered import statements to reflect changes to file structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from './containers/App';
-import UserPage from './containers/UserPage';
-import RepoPage from './containers/RepoPage';
+import {
+  App,
+  UserPage,
+  RepoPage,
+} from './../containers';
 
 export default <Route path="/" component={App}>
   <Route 
